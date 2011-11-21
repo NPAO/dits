0877e1b5d58b118c09a6a3639195a6fd8ac2a332
Wolfgang Kerzendorf <wkerzendorf@gmail.com>
2011-11-20 20:48:58 -0500

    First commit, added basic structure

---
+++ setup.py
@@ -0,0 +1,29 @@
+#setup script for DIT software
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import glob
+
+#version number
+version = '0.01dev'
+
+# Treat everything in scripts except README.rst as a script to be installed
+scripts = glob.glob('scripts/*')
+try:
+	scripts.remove('scripts/README.rst')
+except ValueError:
+	pass
+
+
+setup(name='dits',
+	  description='DIT Software - Pipeline and operation software for the DIT',
+	  author='The DIT collaboration',
+      version=version,
+      packages=['dits'],
+      package_data={'dits': ['data/*']},
+      scripts=scripts
+      )
+      
